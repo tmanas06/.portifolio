fcd7e8c5b03b47a80d5bb221b521d0e8fa18ff73
manas <tmanas2004@gmail.com>
2024-07-29 09:20:23 +0530

    text

--- src/components/cursor/Cursor.jsx
+++ src/components/cursor/Cursor.jsx
@@ -17,6 +17,26 @@ const Cursor = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const handleHoverElements = () => {
+      const hoverElements = document.querySelectorAll("a, button, .hover-effect");
+
+      hoverElements.forEach((element) => {
+        element.addEventListener("mouseenter", handleHover);
+        element.addEventListener("mouseleave", handleUnhover);
+      });
+
+      return () => {
+        hoverElements.forEach((element) => {
+          element.removeEventListener("mouseenter", handleHover);
+          element.removeEventListener("mouseleave", handleUnhover);
+        });
+      };
+    };
+
+    handleHoverElements();
+  }, []);
+
   const handleHover = () => {
     setHovered(true);
   };
@@ -26,15 +46,36 @@ const Cursor = () => {
   };
 
   return (
-    <div className={`custom-cursor ${hovered ? "hovered" : ""}`}
-         style={{ left: `${position.x}px`, top: `${position.y}px` }}>
-      <div className="circle"></div>
+    <div
+      className={`custom-cursor ${hovered ? "hovered" : ""}`}
+      style={{ left: `${position.x}px`, top: `${position.y}px` }}
+    >
+      <div className="arrow">
+        <svg
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12 2L22 12L12 22L2 12L12 2Z"
+            fill="none"
+            stroke="cyan"
+            strokeWidth="2"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M12 2L22 12L12 22L2 12L12 2Z"
+            fill="none"
+            stroke="magenta"
+            strokeWidth="2"
+            strokeLinejoin="round"
+            transform="translate(2,2)"
+          />
+        </svg>
+      </div>
       <div className="ring"></div>
-
-      {/* Optional: Add hover effect listeners */}
-      <div className="hover-listener"
-           onMouseEnter={handleHover}
-           onMouseLeave={handleUnhover}></div>
     </div>
   );
 };
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,52 +2,60 @@ import { useRef } from "react";
 import "./portfolio.scss";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
-const items = [
+const projects = [
   {
     id: 1,
     title: "speech to text application on python and google cloud",
     img: "https://codelabs.developers.google.com/static/codelabs/cloud-speech-text-python3/img/9e7124a578332fed.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
-    link: "https://example.com/project1", // Example link, replace with actual project URL
+    link: "https://github.com/tmanas06/g2210030003", // Example link, replace with actual project URL
   },
   {
     id: 2,
-    title: "Shopping e-commerce apllication",
+    title: "Shopping e-commerce application",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzZIMuFIbiVBDd6BjeDM4a2ApAGBgGxXi6GwEL5V3zlu0vGNgllZ52ZhKA56h-wqd36Tk&usqp=CAU",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    desc: "Shopping Application project created on mern stack.",
+    link: "https://github.com/tmanas06/mern-stack-simple-project",
   },
   {
     id: 3,
     title: "weather application on cli and web",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpJHWwFJPrlJ4XeAdpPP5iWz4iwj4QCYwfzg&s",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    link: "https://github.com/tmanas06/weather_application",
+    desc: "A simple weather application created on html and css.",
   },
   {
     id: 4,
-    title: " Static image application in flutter",
+    title: "Static image application in flutter",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTUmuwZfH6oislpSYTw8StsD1hpszsaLIyiQ&s",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    link: "https://github.com/tmanas06/android_studio_first_app",
+    desc: "This project is a similar project to i_am_rich app which was launched in ios in 2010",
   },
   {
     id: 5,
     title: "Music application with integrated chat feature using MERN stack",
-    img: "https://media.licdn.com/dms/image/D4E22AQFwsEC-KhvOSw/feedshare-shrink_1280/0/1714015692306?e=1721260800&v=beta&t=64oVsV0VCiuMcgxBA7ex-e-REf3uTo0d69RkLW5sAX8",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    img: "https://th.bing.com/th/id/OIP.C85p0nXx8jle1XoKfWCRYAHaDk?rs=1&pid=ImgDetMain",
+    link: "https://github.com/tmanas06/Stellar_society_project",
+    desc: "A space themed music application with integrated chat feature using MERN stack.",
   },
   {
     id: 6,
-    title: " Flight Ticketing Application using NFT (Current Focus)",
-    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSan_m1V52Upb6do4M_APg8LNLC-pUIFek7lA&s",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "To-do-list on Blockchain",
+    img: "https://private-user-images.githubusercontent.com/113188197/305035883-8e5f367c-4882-4b17-b217-6014f632a452.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjIyMjM2OTQsIm5iZiI6MTcyMjIyMzM5NCwicGF0aCI6Ii8xMTMxODgxOTcvMzA1MDM1ODgzLThlNWYzNjdjLTQ4ODItNGIxNy1iMjE3LTYwMTRmNjMyYTQ1Mi5wbmc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQwNzI5JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MDcyOVQwMzIzMTRaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT0xMGYzNDg3YzdkN2I3OTgyYzkwMDI0MWQ3NjliZTRjZTQyYzNmMjk3M2M5NzNlMGM0ZjhmY2YzOWUwZDY4NTA1JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.5PjnA7h0Pe_xAZMLHplSViQ_qGU_rNhvHZ9hszEOeBM",
+    link: "https://github.com/tmanas06/To-do-list-",
+    desc: "A simple to-do-list application on blockchain using remix ide and solidity on VS Code.",
   },
+];
+
+const blogs = [
   {
-    id: 7,
+    id: 1,
     title: "DEEP DIVE INTO COSMWASM CONTRACT STANDARDS: CW20, CW721, CW4, AND CW3",
     img: "https://blogger.googleusercontent.com/img/a/AVvXsEjr1mnzxV7JwmVFbIpTcrMaVGyHqo1y6i8ZJ5-XumXalUlPni-tqGll00s0T8zfJdwRboqhN5eBDHJwDXjG6QASix71TTw_u-6MAg1A3-PzcrRwtnNV6gOnoG-GTG6789dMnKNLsEMErgEUtSL-dg4htAG7pepjCa6LYtk-i32KAtJCUa8jY59cLVnHylE",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
   },
   {
-    id: 8,
+    id: 2,
     title: "ORAICHAIN ARCHITECTURE",
     img: "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg-6fZln_bvZyMUWZBODM8mxeAgvS6pBwx8kiS3dpYG_liMN1o5GS1WIkQF5beRWeQOBh0FxDus1e8I8fAgJmLxjUSyUA9q4Gr5PELqUlpaH1xU9If_WMPM0bAgOLezBl3Aecm9A6s5ti85M0Oul64LN961bywz9oVYruPBV7HjGb4lk_byfejAWO130lA/s320/img2.png",
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
@@ -56,11 +64,9 @@ const items = [
 
 const Single = ({ item }) => {
   const ref = useRef();
-
   const { scrollYProgress } = useScroll({
     target: ref,
   });
-
   const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
 
   return (
@@ -68,14 +74,16 @@ const Single = ({ item }) => {
       <div className="container">
         <div className="wrapper">
           <div className="imageContainer" ref={ref}>
-            <img src={item.img} alt="" />
+            <img src={item.img} alt={item.title} />
           </div>
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <a href={item.link} target="_blank" rel="noopener noreferrer">
-              <button>Check this out</button>
-            </a>
+            {item.link && (
+              <a href={item.link} target="_blank" rel="noopener noreferrer">
+                <button>Check this out</button>
+              </a>
+            )}
           </motion.div>
         </div>
       </div>
@@ -83,14 +91,34 @@ const Single = ({ item }) => {
   );
 };
 
+const Projects = () => {
+  return (
+    <div className="projects">
+      <h2 className="center-title">Projects</h2>
+      {projects.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
+const Blogs = () => {
+  return (
+    <div className="blogs">
+      <h2 className="center-title">Blogs</h2>
+      {blogs.map((item) => (
+        <Single item={item} key={item.id} />
+      ))}
+    </div>
+  );
+};
+
 const Portfolio = () => {
   const ref = useRef();
-
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["end end", "start start"],
   });
-
   const scaleX = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
@@ -102,9 +130,8 @@ const Portfolio = () => {
         <h1 style={{ fontFamily: "'cyberway-riders-font-f43849', sans-serif" }}>Inventory</h1>
         <motion.div style={{ scaleX }} className="progressBar"></motion.div>
       </div>
-      {items.map((item) => (
-        <Single item={item} key={item.id} />
-      ))}
+      <Projects />
+      <Blogs />
     </div>
   );
 };
